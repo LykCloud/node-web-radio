4743e2fbc517689f838e200dd4cf5043f15531d6
LykCloud <145738195+LykCloud@users.noreply.github.com>
2023-11-27 21:55:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-const express         = require('express')
-    , app             = express()
-    , http            = require('http')
-    , path            = require('path')
-    , { Server }      = require('socket.io')
-    , RadioStation    = require('radio-station')
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+const RadioStation = require('radio-station');
 
-const server = http.createServer(app)
-    , io = new Server(server)
+const server = http.createServer(app);
+const io = new Server(server);
 
-const port = 8080
+const port = 8080;
 
-;(async () => {
+(async () => {
   const radio = await RadioStation.create({
     pathWorkDir: path.join(__dirname, 'tracks-data-folder'),
     isLauncher: false,
     mainPort: port
-  })
+  });
 
   await radio.track.loads(
     path.join(__dirname, '/tracks-for-load')
-  )
+  );
 
   app.get('/radio', (req, res) => {
-    radio.addListener(req, res)
-  })
+    radio.addListener(req, res);
+  });
 
-  io.on('connection', async socket => {
-    radio.onUse(info => {
-      socket.emit('onUse', info)
-    })
-  })
+  io.on('connection', async (socket) => {
+    radio.onUse((info) => {
+      socket.emit('onUse', info);
+    });
+  });
 
   app.get('/picture', async (req, res) => {
-    radio.picture(req, res)
-  })
+    radio.picture(req, res);
+  });
 
   app.get('/info', async (req, res) => {
-    radio.info(req, res)
-  })
-})()
+    radio.info(req, res);
+  });
 
-app.use('/', express.static(__dirname + '/public'))
+  // Serve static files
+  app.use('/', express.static(__dirname + '/public'));
 
-server.listen(port, () => {
-  console.log(`open: http://localhost:${port}`)
-})
+  server.listen(port, () => {
+    console.log(`open: http://localhost:${port}`);
+  });
+})();
